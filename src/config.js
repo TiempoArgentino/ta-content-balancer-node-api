1ab5f46e0425f3ca7c4b1a86c9a0553cc4499082
Manuel Paiva <manuelpaiva@Manuels-MacBook-Pro.local>
2021-05-25 10:28:50 -0300

    starter commit

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ config();
 
 export default {
   SECRET: "rest-api-auth",
-  mongodbURL: process.env.MONGO_URI || "mongodb://localhost/companydb",
+  mongodbURL:
+    process.env.MONGO_URI ||
+    "mongodb+srv://amazing-amy:oUxApIArAhadL30W@cluster0.ir1ed.mongodb.net/tiempo-ar?retryWrites=true&w=majority",
 };
