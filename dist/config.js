18fa544548cda61c807a577a62779ad283e361d2
Manuel Paiva <manuelpaiva@Manuels-MacBook-Pro.local>
2021-05-31 14:03:09 -0300

    .env DB

--- dist/config.js
+++ dist/config.js
@@ -10,6 +10,6 @@ var _dotenv = require("dotenv");
 (0, _dotenv.config)();
 var _default = {
   SECRET: "rest-api-auth",
-  mongodbURL: process.env.MONGO_URI || "mongodb://taroot:supertierraS2BR2@190.105.238.93:27017"
+  mongodbURL: process.env.MONGO_URI || "mongodb://localhost/postsapi"
 };
 exports["default"] = _default;
--- src/config.js
+++ src/config.js
@@ -3,7 +3,5 @@ config();
 
 export default {
   SECRET: "rest-api-auth",
-  mongodbURL:
-    process.env.MONGO_URI ||
-    "mongodb://taroot:supertierraS2BR2@190.105.238.93:27017",
+  mongodbURL: process.env.MONGO_URI || "mongodb://localhost/postsapi",
 };
