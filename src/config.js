9551acaaccde48df5a481474158104f9b89852de
Manuel Paiva <manuelpaiva@Manuels-MacBook-Pro.local>
2021-05-31 12:49:49 -0300

    db

--- src/config.js
+++ src/config.js
@@ -3,5 +3,7 @@ config();
 
 export default {
   SECRET: "rest-api-auth",
-  mongodbURL: process.env.MONGO_URI || "mongodb://localhost/postsapi",
+  mongodbURL:
+    process.env.MONGO_URI ||
+    "mongodb://taroot:supertierraS2BR2@190.105.238.93:27017",
 };
